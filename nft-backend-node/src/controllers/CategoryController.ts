5411410ea7aa6feaa967db97563e410c0080b621
HoangDao <donkey_stupid92@yahoo.com>
2022-01-17 16:20:26 +0700

    feat(backend): add ticket categories

--- nft-backend-node/src/controllers/CategoryController.ts
+++ nft-backend-node/src/controllers/CategoryController.ts
@@ -1,9 +1,15 @@
 import { CategoryModel } from '../models';
 
 export const DEFAULT_CATEGORIES: CategoryModel[]  = [
-  { id: '386b738d-fe87-4e5d-afb2-a6fea21daf17', name: 'Korean Idol', tag: 'idol', imageUrl: '', description: 'This is a test category 1' },
-  { id: '4f4bdd02-feac-471c-9969-1d1f8c0bfbcf', name: 'Sport', tag: 'sport', imageUrl: '', description: 'This is a test category 2' },
-  { id: '43c72305-7f53-49d8-a17f-5ce774573a8e', name: 'Musical', tag: 'musical', imageUrl: '', description: 'This is a test category 3' },
+  { id: '386b738d-fe87-4e5d-afb2-a6fea21daf17', name: 'Concert', tag: 'concert', imageUrl: '', description: 'Concert category' },
+  { id: '43c72305-7f53-49d8-a17f-5ce774573a8e', name: 'Camp', tag: 'camp', imageUrl: '', description: 'Camp category' },
+  { id: 'fcdeb7fd-ed14-49c4-9045-a0310ef68a1c', name: 'Workshop', tag: 'workshop', imageUrl: '', description: 'Workshop category' },
+  { id: '735b807b-5bb3-4200-94b1-1eb88886fcb3', name: 'Conference', tag: 'conference', imageUrl: '', description: 'Conference category' },
+  { id: '5abc58eb-4fb3-40fd-89b7-c4b6fed37d4f', name: 'Attractions', tag: 'attractions', imageUrl: '', description: 'Attractions category' },
+  { id: '4f4bdd02-feac-471c-9969-1d1f8c0bfbcf', name: 'Sport', tag: 'sport', imageUrl: '', description: 'Sport category' },
+  { id: '4f4bdd02-feac-471c-9969-1d1f8c0bfbcf', name: 'Movie', tag: 'sport', imageUrl: '', description: 'Movie category' },
+  { id: '4f4bdd02-feac-471c-9969-1d1f8c0bfbcf', name: 'Course', tag: 'sport', imageUrl: '', description: 'Course category' },
+  { id: '080bc6ba-cb4c-47ed-a93f-d5a5563ebd0f', name: 'Event', tag: 'event', imageUrl: '', description: 'Event category' },
 ];
 
 class CategoryController {
